b016873245ab39a40f47d7fb98d207582dfc2c9c
a lan <bluenowte97@gmail.com>
2018-11-15 22:24:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -118,6 +118,6 @@ def organize_schools(schools)
     new_hash[city].push(key)
     new_hash[city].push(schools.key(value))
   end
-   new_hash[:chicago].uniq
+   new_hash[_key].uniq
    new_hash
 end

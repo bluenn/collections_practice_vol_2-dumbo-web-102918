fe6fb18408fd36a8aeec5c47d212a1c862267c7a
a lan <bluenowte97@gmail.com>
2018-11-15 21:23:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -117,7 +117,7 @@ def organize_schools(schools)
   end
   city = new_hash[city]
  schools.each do |key, value|
-   if value.include?(city)
+   if value[:location].include?(city)
      new_hash[city].push(key)
    end
 end

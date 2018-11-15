8ec2d86ce51ce02be9f45127bab2e148ae66c79f
a lan <bluenowte97@gmail.com>
2018-11-15 22:49:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -122,6 +122,6 @@ def organize_schools(schools)
   #new_hash["Chicago"].delete_at(1)
   new_hash["NYC"] = new_hash["NYC"].insert(1,"flatiron school").reverse
   new_hash["SF"] = new_hash["SF"].reverse
-  new_hash["Chicago"] = new_hash["Chicago"].unshift
+  new_hash["Chicago"] = new_hash["Chicago"]*.delete_at(1)
   new_hash
 end
